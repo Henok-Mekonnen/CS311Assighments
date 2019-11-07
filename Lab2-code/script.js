e6c8015c4bd7171a0e6940c16e2340f901bb15d9
Henok-Mekonnen <henok.mekonnen7@gmail.com>
2019-11-07 08:27:29 -0600

    Assighment W3D3

---
+++ Lab2-code/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function checkForSum(numList, num){
+    let m = new Map();
+
+    for (let i = 0; i < numList.length; i++) {
+        m.set(numList[i], num - numList[i]);   
+    }
+
+    for (const [key, value] of m) {
+        if (m.has(value)) {
+            return true;
+        }
+    }
+
+    return false;
+}
